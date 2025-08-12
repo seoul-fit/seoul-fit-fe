9955eb0ce9708b24c4fd7b41faf0d549490a1f98
yusgarya <spica4345@gmail.com>
2025-08-12 18:11:42 +0900

    fix: 무더위 쉼터 be endpoinrt 변경에 따른 수정

--- app/api/cooling-shelter/route.ts
+++ app/api/cooling-shelter/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const response = await fetch('http://localhost:8080/api/v1/cooling-shelter/all', {
+        const response = await fetch('http://localhost:8080/api/v1/cooling-shelters/all', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
